c3d9ed770c695522cf872a81e362a3b29c9f99fe
Caio Reis <caio@cyclumsn.com>
2015-02-04 17:42:02 -0200

    Creating Image object

--- kraken.js
+++ kraken.js
@@ -1,59 +1,53 @@
 var Kraken = Npm.require('kraken');
 
-kraken = new Kraken({
-  api_key: '31eb328419776902d4087e5afc55c45b',
-  api_secret: '791ab19e749368b544833304573ab7d1e94984b3'
-});
+var kraken;
+var s3credentials = {};
+var sizes = {};
 
-sizes = {
-  thumb: {
-    name: "thumb",
-    width: 250,
-    height: 220,
-    strategy: 'exact'
+Images = {
+  setup: function (credentials) {
+    
+    s3credentials = credentials.s3;
+
+    kraken = new Kraken({
+      api_key: credentials.kraken.key,
+      api_secret: credentials.kraken.secret
+    });
   },
-  medium: {
-    name: "medium",
-    width: 577,
-    height: 307,
-    strategy: 'exact'
+  setSizes: function (imageSizes) {
+    sizes = imageSizes;
   },
-  normal: {
-    name: "normal",
-    width: 1200,
-    height: 1200,
-    strategy: 'auto'
+  resize: function (url, path, version, callback) {
+
+    var opts = {
+      url: url,
+      convert: {
+        format: "jpeg",
+        background: "#000000"
+      },
+      wait: true,
+      lossy: true,
+      resize: {
+        width: sizes[version].width,
+        height: sizes[version].height,
+        strategy: sizes[version].strategy,
+        background: "#000000"  
+      },
+      s3_store: {
+        key: s3credentials.key,
+        path: path + '_' + sizes[version].name + '.jpg',
+        secret: s3credentials.secret,
+        bucket: s3credentials.bucket,
+        region: s3credentials.region
+      }
+    };
+
+    kraken.url(opts, function(data) {
+      if (!data.success) {
+        console.log(data);
+      }
+      callback(data);
+    });
   }
-};
-
-resizeKraken = function (url, path, version, callback) {
-  var opts = {
-    url: url,
-    convert: {
-      format: "jpeg",
-      background: "#000000"
-    },
-    wait: true,
-    lossy: true,
-    resize: {
-      width: sizes[version].width,
-      height: sizes[version].height,
-      strategy: sizes[version].strategy,
-      background: "#000000"
-    },
-    s3_store: {
-      key: 'AKIAJBPPGSKGZE5ARP6Q',
-      path: path + '_' + sizes[version].name + '.jpg',
-      secret: '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',
-      bucket: 'ab-fotos',
-      region: 'us-west-2'
-    }
-  };
-
-  kraken.url(opts, function(data) {
-    if (!data.success) {
-      console.log(data);
-    }
-    callback(data);
-  });
 }
+
--- package.js
+++ package.js
@@ -1,22 +1,24 @@
 Package.describe({
   name: 'kraken',
   summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
+  version: '1.0.1',
   git: ' /* Fill me in! */ '
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
   api.addFiles('kraken.js', ['server']);
-   api.export('resizeKraken');
+  api.export('Images', ['server']);
 });
-
-// Package.onTest(function(api) {
-//   api.use('tinytest');
-//   api.use('kraken');
-//   api.addFiles('kraken-tests.js');
-// });
 
 Npm.depends({
 	kraken: "0.2.1"
 });
+
+/* Use case
+
+Images.resize('http://images.visitcanberra.com.au/images/canberra_hero_image.jpg', 'teste', 'thumb', function (argument) {
+  console.log(argument);
+});
+
+*/
